bc0add201cbae7bcbfa915ffa37ccbd925d7b702
arohi1208 <arohimishra03bzu@gmail.com>
2025-11-12 09:59:36 +0530

    commit 02

---
+++ target/run/mkmall/models/stg_customers.sql
@@ -0,0 +1,29 @@
+
+create or replace transient table analytics.arohi.stg_customers
+    
+    
+    
+    as (select 
+
+   c_custkey as customer_id,
+
+   c_nationkey as c_nation_id,
+
+   c_name as name,
+
+   c_address as address,
+
+   c_phone as phone_number,
+
+   c_acctbal as account_balance,
+
+   c_mktsegment as market_segment,
+
+   c_comment as comment
+
+from SOURCEDB.MK_MALL.CUSTOMERS
+    )
+;
+
+
+  
